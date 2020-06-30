cbd0954de4f3c3cfea8dd428e5e023781a88ae25
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-06-30 00:21:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,11 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  if hash[key] 
+    hash[key]
+  else
+    nil
+  end
 end
 
 def update_counting_hash(hash, key)
